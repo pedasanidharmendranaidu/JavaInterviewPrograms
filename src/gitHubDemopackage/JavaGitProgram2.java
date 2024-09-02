70c3cd3aa00677b569d889542ca26169a54c2cad
pedasanidharmendranaidu <dharmamax01@gmail.com>
2024-09-02 15:21:36 +0530

    successful

--- src/gitHubDemopackage/JavaGitProgram2.java
+++ src/gitHubDemopackage/JavaGitProgram2.java
@@ -3,5 +3,12 @@ package gitHubDemopackage;
 public class JavaGitProgram2 {
     public static void main(String[] args) {
         System.out.println("*************");
+        System.out.println("hiii im from gitHub:::!!!!");
+        System.out.println("edited from dummi branch");
+        System.out.println("hiii im from gitHub:::!!!!");
+        System.out.println("hiii im from gitHub:::!!!!");
+        System.out.println("edited from dummi branch");
+        System.out.println("hiii im from gitHub:::!!!!");
+
     }
 }
