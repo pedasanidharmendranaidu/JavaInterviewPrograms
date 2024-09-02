c50e19b8f15fdafe6c8271dd3b345dbf83d0e7d0
dharmendranaidu <131648339+pedasanidharmendranaidu@users.noreply.github.com>
2024-09-02 15:07:31 +0530

    Update JavaGitProgram2.java

--- src/gitHubDemopackage/JavaGitProgram2.java
+++ src/gitHubDemopackage/JavaGitProgram2.java
@@ -3,5 +3,6 @@ package gitHubDemopackage;
 public class JavaGitProgram2 {
     public static void main(String[] args) {
         System.out.println("*************");
+        System.out.println("second line manually edited");
     }
 }
